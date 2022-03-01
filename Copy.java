807d52142f2cfbf11404ccd891f000675b141ff6
sandeep bhingle <sandeepbhingle@gmail.com>
2022-03-01 10:58:56 +0530

    Datatypes

---
+++ Copy.java
@@ -0,0 +1,8 @@
+class Copy {
+    static public void main(String temp[]) {
+        System.out.println("Hello String");
+		/*for (String str:temp) {
+			System.out.print(str+" ");
+		}*/
+    }
+}
---
+++ DatatypeDemos.java
@@ -0,0 +1,20 @@
+public class DatatypeDemos {
+    public static void main(String[] args) {
+        byte value = 127;
+        short shortValue = 32767;
+        int salary = Integer.MAX_VALUE;
+        long population = Long.MAX_VALUE-1;
+        float temp = 33.3f;
+        double bankBal = 4555558900978.98989d;
+        char ch = 'A';
+        boolean boolValue = true;
+
+        System.out.println(value);
+        System.out.println(shortValue);
+        System.out.println(salary);
+        System.out.println(population);
+        System.out.println("Char demos");
+        System.out.println('A'+'A');
+    }
+    
+}
