c605798bef1d1ce402b3dba33d05e2aa31063079
sandeep bhingle <sandeepbhingle@gmail.com>
2022-05-20 09:42:12 +0530

    Multithreading

---
+++ src/multithreading/Demo1.java
@@ -0,0 +1,38 @@
+package multithreading;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Task task = new Task();
+        task.setName("taskThread1");
+        task.start();
+
+        Task task2 = new Task();
+        task2.setName("taskThread2");
+        task2.start();
+
+
+        // main method is the job of main thread
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
+        //Another way to create thread.
+        Runnable runnable = ()->{
+            for (int i = 0; i <9 ; i++) {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+
+        Thread thread = new Thread(runnable,"LambdaThread");
+        thread.start();
+    }
+}
+
+class Task extends Thread {
+    @Override
+    //job of custom thread.
+    public void run() {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
